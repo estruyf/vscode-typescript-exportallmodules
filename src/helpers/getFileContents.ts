1d9fceb073c168f19098ff927f0ba3273c57e894
Elio Struyf <elio@struyfconsulting.be>
2024-07-03 09:52:45 +0200

    Barrel files import

--- src/helpers/getFileContents.ts
+++ src/helpers/getFileContents.ts
@@ -1,18 +1,18 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
+import { ExportAll } from "../commands";
 
 export const getFileContents = (filePath: string): string => {
-  const barrelFiles = ['index.ts', 'index.tsx'];
   if (fs.lstatSync(filePath).isDirectory()) {
-    for (const indexFile of barrelFiles) {
+    for (const indexFile of ExportAll.barrelFiles) {
       const indexPath = path.join(filePath, indexFile);
       if (fs.existsSync(indexPath)) {
-        return fs.readFileSync(indexPath, 'utf8');
+        return fs.readFileSync(indexPath, "utf8");
       }
     }
-    return '';
+    return "";
   } else if (fs.lstatSync(filePath).isFile()) {
-    return fs.readFileSync(filePath, 'utf8');
+    return fs.readFileSync(filePath, "utf8");
   }
-  return '';
+  return "";
 };
