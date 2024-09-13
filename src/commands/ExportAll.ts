e579127c9a86b7f21b1f8212b345ec8f0a04fc5d
Bruno Paré-Simard <bruno.paresimard@gmail.com>
2024-09-13 11:01:50 -0400

    Add `index` for folder export with extension provided

--- src/commands/ExportAll.ts
+++ src/commands/ExportAll.ts
@@ -132,6 +132,11 @@ export class ExportAll {
               ? fileExtension
               : `.${fileExtension}`;
           }
+          if (fileExtension && item.type === "folder") {
+            fileSuffix = fileExtension.startsWith(`.`)
+              ? `/index${fileExtension}`
+              : `/index.${fileExtension}`;
+          }
 
           if (namedExports) {
             const filePath = join(uri.fsPath, item.name);
